88ab35a1203dd261d133947de94a73e816aa76e8
huyenngau <huyenn.230197@gmail.com>
2020-04-07 23:40:54 +0700

    feat: Build a Queue by using linked list class

---
+++ data_structures/queue/queue_using_llist_class.py
@@ -0,0 +1,156 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self, value):
+        """ Prepend a node to the beginning of the list """
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
+
+    def append(self, value):
+        """ Append a node to the end of the list """
+        # Here I'm not keeping track of the tail. It's possible to store the tail
+        # as well as the head, which makes appending like this an O(1) operation.
+        # Otherwise, it's an O(N) operation as you have to iterate through the
+        # entire list to add a new tail.
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def search(self, value):
+        """ Search the linked list for a node with the requested value and return the node. """
+        if self.head is None:
+            return None
+
+        node = self.head
+        while node:
+            if node.value == value:
+                return node
+            node = node.next
+
+        raise ValueError("Value not found in the list.")
+
+    def remove(self, value):
+        """ Delete the first node with the desired data. """
+        if self.head is None:
+            return
+
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+
+        node = self.head
+        while node.next:
+            if node.next.value == value:
+                node.next = node.next.next
+                return
+            node = node.next
+
+        raise ValueError("Value not found in the list.")
+
+    def pop(self):
+        """ Return the first node's value and remove it from the list. """
+        if self.head is None:
+            return None
+
+        node = self.head
+        self.head = self.head.next
+
+        return node.value
+
+    def insert(self, value, pos):
+        """ Insert value at pos position in the list. If pos is larger than the
+            length of the list, append to the end of the list. """
+        if pos == 0:
+            self.prepend(value)
+            return
+
+        index = 0
+        node = self.head
+        while node.next and index <= pos:
+            if (pos - 1) == index:
+                new_node = Node(value)
+                new_node.next = node.next
+                node.next = new_node
+                return
+
+            index += 1
+            node = node.next
+        else:
+            self.append(value)
+
+    def size(self):
+        """ Return the size or length of the linked list. """
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+    def to_list(self):
+        out = []
+        node = self.head
+        while node:
+            out.append(node.value)
+            node = node.next
+        return out
+
+
+class Queue_Solution:
+    def __init__(self):
+        # Initialize the Queue
+        self.linkedlist = LinkedList()
+
+    def size(self):
+        # Check the size of the Queue
+        return self.linkedlist.size()
+
+    def enqueue(self, item):
+        # Enter item into Queue
+        self.linkedlist.append(item)
+
+    def dequeue(self):
+        # Remove item from the Queue
+        return self.linkedlist.pop()
+
+
+# Setup
+q = Queue_Solution()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
