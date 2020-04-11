585cf503d561099eb6cb9df6871ae6eced2b558a
huyenngau <huyenn.230197@gmail.com>
2020-04-11 23:14:22 +0700

    fix: Update code to Week 2 assignment - 2nd assignment, update prime_number_in_nth method

--- week_2_assignment/prime_numbers.py
+++ week_2_assignment/prime_numbers.py
@@ -1,28 +1,32 @@
 # check a number is prime or not
 def is_prime_number(number):
+    if number < 2:
+        return False
+
     for i in range(2, number):
         if number % i == 0:
             return False
+
     return True
 
 
 def prime_number_in_nth(nth):
+    if nth == 1:
+        return 2
+
     number = 2
     prime_index = 1
-    import time
-    start_time = time.time()
-    while prime_index <= nth:
+
+    while prime_index < nth:
         number = number + 1
         if is_prime_number(number):
             prime_index += 1
+        if prime_index == nth:
+            return number
 
-    end_time = time.time()
-    print(end_time - start_time)
-    return number
+    return None
 
 
-# bool_value = is_prime_number(9)
-# print(bool_value)
-
+# Test
 test_number = prime_number_in_nth(10001)
 print(test_number)
