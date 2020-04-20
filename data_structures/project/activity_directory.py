b87a4f4304a9ca356267a46306c364f8a7c32436
huyenngau <huyenn.230197@gmail.com>
2020-04-21 00:16:30 +0700

    feat: Data structures - Project 4: Active Directory

---
+++ data_structures/project/activity_directory.py
@@ -0,0 +1,50 @@
+class Group(object):
+    def __init__(self, name):
+        self.name = name  # group name
+        self.groups = []  # list of child groups
+        self.users = []  # list of user_names/ids
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    if user in group.get_users():
+        return True
+    for group in group.get_groups():
+        is_user_in_group(user, group)
+    return False
+
+
+# Test case
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child.add_group(sub_child)
+parent.add_group(child)
+
+
+print(is_user_in_group("sub_child_user", sub_child))
