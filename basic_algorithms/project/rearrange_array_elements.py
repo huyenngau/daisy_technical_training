93aa77bfad9bfe0af011321011787dd9daf487ed
huyenngau <huyenn.230197@gmail.com>
2020-05-10 16:52:26 +0700

    feat: Basic Algorithms - Final project: Rearrange array elements

---
+++ basic_algorithms/project/rearrange_array_elements.py
@@ -0,0 +1,77 @@
+def mergesort(items):
+    if len(items) <= 1:
+        return items
+
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+
+    left = mergesort(left)
+    right = mergesort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+
+    return merged
+
+
+def rearrange_digits(input_list):
+    """
+    Rearrange Array Elements so as to form two number such that their sum is maximum.
+
+    Args:
+       input_list(list): Input List
+    Returns:
+       (int),(int): Two maximum sums
+    """
+    sorted_input_list = mergesort(input_list)
+
+    n = len(sorted_input_list)
+
+    # The first number is filled with digits at the even indices of the sorted input list
+    x = 0
+    for i in range(0, n, 2):
+        x = x * 10 + sorted_input_list[i]
+
+    # The second number is filled with digits at the odd indices of the sorted input list
+    y = 0
+    for k in range(1, n, 2):
+        y = y * 10 + sorted_input_list[k]
+
+    return x, y
+
+
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+# Test cases
+test_case_1 = [[1, 2, 3, 4, 5], [542, 31]]
+test_function(test_case_1)
+
+test_case_2 = [[1, 2, 3, 4, 5], [531, 42]]
+test_function(test_case_2)
+
+test_case_3 = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function(test_case_3)
---
+++ basic_algorithms/project/rerrange_array_elements_explanation.md
@@ -0,0 +1,16 @@
+1. Using Mergesort to sort the input_list because Mergesort has time complexity is O(n log n)
+Init 2 variables x, y are equal to 0. 
+x is filled with digits at the even indices of the sorted input list.
+y is filled with digits at the odd indices of the sorted input list. 
+(x, y) is output of problem. 
+
+This solution makes The number of digits in both the numbers cannot differ by more than 1.
+
+2. 
+- Time complexity:
+     + Mergesort: O(n log n)
+     + Find two numbers such that their sum is maximum: O(n)
+     Total: O(n log n)
+ 
+- Space Complexity: O(1).
+Constant extra space is needed.
