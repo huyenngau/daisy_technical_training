7a2d041ef1af2448bd62e414e716f896aa4be374
huyenngau <huyenn.230197@gmail.com>
2020-05-12 22:55:14 +0700

    fix: Make split_path is function

--- basic_algorithms/project/httprouter_using_a_trie.py
+++ basic_algorithms/project/httprouter_using_a_trie.py
@@ -54,7 +54,7 @@ class Router:
     def add_handler(self, path, handler):
         # Add a handler for a path
         # Split the path and pass the pass parts as a list to the RouteTrie
-        child_paths = self.split_path(path)
+        child_paths = split_path(path)
         self.root.insert(child_paths, handler)
 
     def lookup(self, path):
@@ -62,20 +62,21 @@ class Router:
         # return the "not found" handler if it's not found or
         # bonus points if a path works with and without a trailing slash
         # e.g. /about and /about/ both return the /about handler
-        child_paths = self.split_path(path)
+        child_paths = split_path(path)
         handler = self.root.find(child_paths)
         if handler:
             return handler
         else:
             return self.not_found
 
-    def split_path(self, path):
-        # Need to split the path into parts for
-        # both the add_handler and lookup functions,
-        child_paths = path.split("/")
-        if path.endswith("/"):
-            return child_paths[:-1]
-        return child_paths
+
+def split_path(path):
+    # Need to split the path into parts for
+    # both the add_handler and lookup functions,
+    child_paths = path.split("/")
+    if path.endswith("/"):
+        return child_paths[:-1]
+    return child_paths
 
 
 # Here are some test cases and expected outputs
